0fd24f8359c0fa51bd20f513da9ce1a59860357f
Linka-hub <eldrickarsy9@gmail.com>
2026-01-18 16:47:38 +0700

    Update main.ts

--- day4-Backend/src/main.ts
+++ day4-Backend/src/main.ts
@@ -1,36 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
 
-  //  aktifkan validation 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-
+  // Swagger config
   const config = new DocumentBuilder()
     .setTitle('Event Simple Storage DApp API')
-    .setDescription('The Simple Storage dApp Web3 API documentation With NestJS and Swagger')
+    .setDescription('The Simple Storage dApp Web3 API documentation')
     .setVersion('1.0')
-    .addTag(' Author : Eldrick Arsy L. 251011401100')
+    .addTag('Author : Eldrick Arsy Listyannika')
     .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('myswagger', app, documentFactory);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('myswagger', app, document);
 
   const port = process.env.PORT || 3000;
-await app.listen(port);
+  await app.listen(port);
 
-
+  console.log(`🚀 Server running on port ${port}`);
 }
 
 bootstrap().catch((error) => {
