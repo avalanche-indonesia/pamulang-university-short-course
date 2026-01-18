e781225fc50567133b0f98ccca83ebb3a7b334fc
Linka-hub <eldrickarsy9@gmail.com>
2026-01-18 17:29:38 +0700

    update

--- day4-Backend/src/main.ts
+++ day4-Backend/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('myswagger', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 Server running on port ${port}`);
 }
